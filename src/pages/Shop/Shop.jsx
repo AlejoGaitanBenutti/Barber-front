601a60929ed5d889706b7ba6aeeedb490304126e
AlejoGaitanBenutti <alejogbs8@gmail.com>
2025-08-05 17:11:24 -0300

    add spiner

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,52 +1,55 @@
-import React from 'react';
-import styles from "../Shop/Shop.module.css"
+import React, { useState, useEffect } from 'react';
+import styles from "../Shop/Shop.module.css";
 import CardProducto from '../../components/CardProducto';
-import { useState, useEffect } from 'react';
 import { getAllProductos } from '../../services/productServices';
 
+function Shop() {
+  const [productos, setProductos] = useState([]);
+  const [loading, setLoading] = useState(true); // Estado para el spinner
 
-function Shop(){
+  useEffect(() => {
+    const fetchProductos = async () => {
+      try {
+        const data = await getAllProductos();
+        setProductos(data);
+      } catch (error) {
+        console.error("Error cargando productos", error);
+      } finally {
+        setLoading(false); // Termina la carga
+      }
+    };
 
-    const [productos, setProductos] = useState([]);
+    fetchProductos();
+  }, []);
 
-    useEffect(()=>{
-
-        const fetchProductos = async () =>{
-            const data = await getAllProductos();
-
-            setProductos(data);
-        }
-
-        fetchProductos();
-    }
-    ,[])
-
-    return(
-
-    <div style={{borderBottom:"2px solid white"}} >
-        <div className={styles.heroShop}>
-            <h1 className={styles.heroShopTitle}>EXPLORA NUESTROS PRODUCTOS</h1>
-        </div>
-        <div className={styles.containerShop}>
-            <div className={styles.containerCard}>
+  return (
+    <div style={{ borderBottom: "2px solid white" }}>
+      <div className={styles.heroShop}>
+        <h1 className={styles.heroShopTitle}>EXPLORA NUESTROS PRODUCTOS</h1>
+      </div>
+      <div className={styles.containerShop}>
+        {/* Mostrar spinner mientras carga */}
+        {loading ? (
+          <div className={styles.spinnerContainer}>
+            <div className={styles.spinner}></div>
+          </div>
+        ) : (
+          <div className={styles.containerCard}>
             {productos.map((producto, index) => (
-                 <div
-                        key={producto.id}
-                        data-aos="fade-up"
-                        data-aos-duration="800"
-                        data-aos-delay={index * 100}
-                    >
-                        <CardProducto producto={producto} />
-                </div>
-                ))}
-            </div>
-        </div>
-
+              <div
+                key={producto.id}
+                data-aos="fade-up"
+                data-aos-duration="800"
+                data-aos-delay={index * 100}
+              >
+                <CardProducto producto={producto} />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
-
-    )
+  );
 }
 
 export default Shop;
-
-
