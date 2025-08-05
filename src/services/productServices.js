43e5eb38dc233636f83028609b77e01ab6e00dc9
AlejoGaitanBenutti <alejogbs8@gmail.com>
2025-08-05 12:25:12 -0300

    fix en services , evitar problemas de parseo

--- src/services/productServices.js
+++ src/services/productServices.js
@@ -1,27 +1,26 @@
 const apiUrl = import.meta.env.VITE_API_URL;
 
-export const getAllProductos =  async () =>{
-    try{
-    const response = await fetch(apiUrl,{
-        method:"GET",
-        headers:{
-            "Content-Type": "application/json",
-        },
-         mode: "cors",
-         credentials:"include"
-    
+export const getAllProductos = async () => {
+  try {
+    const response = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
+      credentials: "include"
     });
 
-    const text = await response.text();
-    
-
-    const data= JSON.parse(text);
-    return data
-
-
-    }catch(e){
-        console.error("Error al obtener productos", e);
-        return [];
-        
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-}
+
+    // En lugar de leer como texto y parsear, leé directo JSON
+    const data = await response.json();
+    return data;
+
+  } catch (e) {
+    console.error("Error al obtener productos", e);
+    return [];
+  }
+};
